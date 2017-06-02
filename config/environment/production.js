a6dcc195c9f0f788ad32f18fc9658d800ebdecee
Prashantt <pygold@gmail.com>
2017-06-02 08:44:44 +0530

    mongoose reconnect

--- config/environment/production.js
+++ config/environment/production.js
@@ -3,21 +3,28 @@
 // Production specific configuration
 // =================================
 module.exports = {
-  // Server IP
-  ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
-            undefined,
+    // Server IP
+    ip: process.env.OPENSHIFT_NODEJS_IP ||
+        process.env.IP ||
+        undefined,
 
-  // Server port
-  port:     process.env.OPENSHIFT_NODEJS_PORT ||
-            process.env.PORT ||
-            8080,
+    // Server port
+    port: process.env.OPENSHIFT_NODEJS_PORT ||
+        process.env.PORT ||
+        8080,
 
-  // MongoDB connection options
-  mongo: {
-    uri:    process.env.OPENSHIFT_MONGODB_DB_URL ||
-            'mongodb://pygold:14021992@ds163940.mlab.com:63940/finemart'
-  },
+    // MongoDB connection options
+    mongo: {
+        uri: process.env.OPENSHIFT_MONGODB_DB_URL ||
+            'mongodb://pygold:14021992@ds163940.mlab.com:63940/finemart',
 
-  seedDB: true
+        options: {
+            server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+            replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+        },
+
+
+    },
+
+    seedDB: true
 };
